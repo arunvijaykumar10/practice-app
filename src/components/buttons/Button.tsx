ad68ca1dba4f2f2de2fd33acf6bf146d20f0d62c
arunvijaykumar10 <vjvkarun@gmail.com>
2024-07-23 17:11:48 +0530

    Button Component created

---
+++ src/components/buttons/Button.tsx
@@ -0,0 +1,12 @@
+import "./Button.css";
+
+interface Props {
+  label: string;
+  type: string;
+}
+function Button(props: Props) {
+  const { label, type } = props;
+  return <button className={`btn ${type}`}>{label}</button>;
+}
+
+export default Button;
